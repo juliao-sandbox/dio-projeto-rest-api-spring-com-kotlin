c6b4e1fdac94888797a5c97bd3ef975d2720b4e3
Juliao F. <juliao.gf@gmail.com>
2023-10-27 14:50:50 -0300

    feat: Anotações adicionadas

--- credit-application-system/src/main/kotlin/me/dio/credit/entity/Credit.kt
+++ credit-application-system/src/main/kotlin/me/dio/credit/entity/Credit.kt
@@ -1,16 +1,33 @@
 package me.dio.credit.entity
 
+import jakarta.persistence.*
 import me.dio.credit.enumeration.Status
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.util.UUID
 
+@Entity
 data class Credit (
+    @Column(nullable = false, unique = true)
     val creditCode: UUID = UUID.randomUUID(),
+
+    @Column(nullable = false)
     val creditValue: BigDecimal = BigDecimal.ZERO,
+
+    @Column(nullable = false)
     val dayFirstInstallment: LocalDate,
+
+    @Column(nullable = false)
     var numberOfInstallments: Int = 0,
+
+    @Enumerated
+    @Column(nullable = false)
     val status: Status = Status.IN_PROGRESS,
+
+    @ManyToOne
     val customer: Customer? = null,
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
     val id: Long? = null
 )
