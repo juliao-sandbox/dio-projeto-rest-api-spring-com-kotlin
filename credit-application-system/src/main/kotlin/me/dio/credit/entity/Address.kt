bdadff783b07ba8a128488ced14e8e99c71b3c7b
Juliao F. <juliao.gf@gmail.com>
2023-10-27 13:30:02 -0300

    feat: Classes iniciais

---
+++ credit-application-system/src/main/kotlin/me/dio/credit/entity/Address.kt
@@ -0,0 +1,6 @@
+package me.dio.credit.entity
+
+data class Address(
+    var zipCode: String = "",
+    var street: String = ""
+)
---
+++ credit-application-system/src/main/kotlin/me/dio/credit/enumeration/Status.kt
@@ -0,0 +1,7 @@
+package me.dio.credit.enumeration
+
+enum class Status {
+    IN_PROGRESS,
+    APPROVED,
+    REJECT
+}
